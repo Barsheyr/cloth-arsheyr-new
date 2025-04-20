019b70204a7e53e4d3e5c27264aa046c57b14eaf
Bashir Amokomowo <barsheyr@gmail.com>
2025-04-20 12:01:29 +0100

    push

---
+++ config/db.js
@@ -0,0 +1,32 @@
+// user name = barsheyr
+// password = xBnuy24BJJRwtZcg
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/ago`, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectDB;
